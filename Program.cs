68676183fa3b96ef780b9ea719b28854cec92284
Pat <113007904+PattZac@users.noreply.github.com>
2023-09-11 11:16:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponvaljanje4Dzad6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi broj: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            double numb = 0;
+            double numc = 3;
+            for(int i=1; i<=n; i++)
+            {
+                numb += 1 / Math.Pow(3,i);
+            }
+            Console.WriteLine(numb);
+            Console.ReadLine();
+        }
+    }
+}
